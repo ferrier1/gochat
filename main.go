f2394daebccf8f5a4104dc2c101e4f63667a4fe6
johnnydrama92 <james.ferrier01@gmail.com>
2018-11-12 22:47:29 +0000

    main func

--- main.go
+++ main.go
@@ -24,3 +24,20 @@ type Message struct {
   Username string `json:"username"`
   Message string `json:"message"`
 }
+
+
+func main() {
+  // fileserver
+  fs := http.Fileserver(http.Dir("../public"))
+  http.Handle("/", fs)
+  // websocket
+  http.HandleFunc("/ws", handleConnections)
+  // listen for incoming messages
+  go HandleMessages()
+  // start server on localhost:8000 with logging
+  log.Println("http server started on :8000")
+  err := http.ListenAndServe(":8000", nil)
+  if err != nil {
+    log.Fatall("ListenAndServe: ", err)
+  }
+}
